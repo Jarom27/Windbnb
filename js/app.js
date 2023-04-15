c07ae67b41ad37619b2bc41f5cb5617fa4347eda
Jarom Mariscal <jaromero_marz234@outlook.com>
2023-04-15 11:46:18 -0700

    Estilos de las cards finalizados

--- js/app.js
+++ js/app.js
@@ -2,4 +2,11 @@ import data from './data.js';
 import dom from './dom.js';
 
 const stays = await data.getData();
+const $location_search_bar = document.getElementById("location");
+const $guest_search_bar = document.getElementById("guests");
+const $modal_search =document.getElementById("modal_search");
+
+$modal_search.addEventListener('shown.bs.modal',e=>{
+    $location_search_bar.focus()
+})
 dom.showCards(stays)
--- js/dom.js
+++ js/dom.js
@@ -5,22 +5,22 @@ con la manipulación del DOM en la aplicación
 const $cards_container = document.getElementById("cards-container");
 const createNewCard = (stay)=>{
     let $card = document.createElement("div");
-    $card.className = "card d-flex flex-column col col-md-4 border-0"
+    $card.className = "card d-flex flex-column col-12 col-md-4 border-0 pb-5"
     $card.innerHTML = `
         <div class="img-object-fit card-img-top">
             <img class="object-fit-cover rounded-4  w-100 h-100" src="${stay.photo}">
         </div>
         <div class="ps-0 container mt-4 d-flex justify-content-between">
-            ${stay.superHost ? '<p class="text-uppercase border py-2 px-3 rounded-pill">super host</p>':''}
+            ${stay.superHost ? '<p class="text-uppercase border py-2 px-3 rounded-pill fs-5">super host</p>':''}
             <div class="py-2">
-                <p class="text-secondary-emphasis fs-6 text-opacity-75 fw-semibold">${stay.type} ${stay.beds !=null ? `, ${stay.beds} beds` : ""}</p>
+                <p class="text-secondary-emphasis fs-5 text-opacity-75 fw-semibold">${stay.type} ${stay.beds !=null ? `, ${stay.beds} beds` : ""}</p>
             </div>
             <div class="d-flex flex-row">
-                <span class="py-2 material-symbols-outlined text-danger">star</span> 
-                <p class="py-2">${stay.rating}</p>
+                <span class=" material-symbols-outlined text-danger">star</span> 
+                <p class="py-2 fs-5">${stay.rating}</p>
             </div>
         </div>
-        <h2 class="fs-6 fw-semibold text-start">${stay.title}</h2>
+        <h2 class="fw-semibold text-start fs-4">${stay.title}</h2>
     `
     return $card
 }
